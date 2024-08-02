45669b4636ade93b3261b1e119cf27e6869cac64
Jonathan Moran <tatangamess@gmail.com>
2024-08-01 20:32:47 -0600

    panlla 2

---
+++ NetGoApp/Controladores/Login/VerificacionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VerificacionViewController.swift
+//  NetGoApp
+//
+//  Created by Jonathan  Moran on 1/8/24.
+//
+
+import UIKit
+
+class VerificacionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
